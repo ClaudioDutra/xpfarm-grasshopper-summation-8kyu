e4976d6178aac20f03b01d38f61d991db3f0b06e
Claudio Bandeira Dutra <cbd@br.ibm.com>
2023-03-24 10:17:51 -0300

    refactor(Text typos): fix typo on test cases names

--- test/summation.test.js
+++ test/summation.test.js
@@ -3,9 +3,9 @@ const { summation } = require("../src/summation");
 describe("Validating the summation", () => {
   test.each([
     [1, 1],
-    [2, 3],
-    [8, 36],
-  ])("should return %i when the number is %i", (number, result) => {
+    [3, 2],
+    [36, 8],
+  ])("should return %i when the number is %i", (result, number) => {
     expect(summation(number)).toBe(result);
   });
 });
